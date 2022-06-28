375ea3747407199e70a23e8a5c19041a797807e5
gzzyyxh <gzzyyxh@gmail.com>
2022-06-28 20:13:33 +0800

    6.28

--- poseutil.py
+++ poseutil.py
@@ -1,44 +1,32 @@
 import cv2
 import mediapipe as mp
-import math
+import time
 
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+mpDraw = mp.solutions.drawing_utils
 
-class PoseDetector():
-    '''
-    人体姿势检测类
-    '''
-    def __init__(self,
-                 static_image_mode=False,
-                 upper_body_only=False,
-                 smooth_landmarks=True,
-                 min_detection_confidence=0.5,
-                 min_tracking_confidence=0.5):
-        '''
-        初始化
-        :param static_image_mode: 是否是静态图片，默认为否
-        :param upper_body_only: 是否是上半身，默认为否
-        :param smooth_landmarks: 设置为True减少抖动
-        :param min_detection_confidence:人员检测模型的最小置信度值，默认为0.5
-        :param min_tracking_confidence:姿势可信标记的最小置信度值，默认为0.5
-        '''
-        self.static_image_mode = static_image_mode
-        self.upper_body_only = upper_body_only
-        self.smooth_landmarks = smooth_landmarks
-        self.min_detection_confidence = min_detection_confidence
-        self.min_tracking_confidence = min_tracking_confidence
-        # 创建一个Pose对象用于检测人体姿势
-        self.pose = mp.solutions.pose.Pose(self.static_image_mode, self.upper_body_only, self.smooth_landmarks,
-                                           self.min_detection_confidence, self.min_tracking_confidence)
-    def find_pose(self, img):
-        '''
-        检测姿势方法
-        :param img: 一帧图像
-        :param draw: 是否画出人体姿势节点和连接图
-        :return: 处理过的图像
-        '''
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # pose.process(imgRGB) 会识别这帧图片中的人体姿势数据，保存到self.results中
-        self.results = self.pose.process(imgRGB)
-        return img
-    def draw_pose(self, img):
-        mp.solutions.drawing_utils.draw_landmarks(img, self.results.pose_
+#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+pTime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    print(results.pose_landmarks)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+    for id, lm in enumerate(results.pose_landmarks.landmark):
+        h, w,c = img.shape
+        print(id, lm)
+        cx, cy = int(lm.x*w), int(lm.y*h)
+        cv2.circle(img, (cx, cy), 5, (255,0,0), cv2.FILLED)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (50,50), cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0), 3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
